8d8cd6bfb0c83aeb5f24ab74c6d84e7ea98e59e9
Pablo <pabloignaciotapia@gmail.com>
2015-05-29 21:00:28 -0300

    test

--- src/test/java/process/AnalisisCeldas.java
+++ src/test/java/process/AnalisisCeldas.java
@@ -1,10 +1,11 @@
 package process;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AnalisisCeldas {
 
-	int matriz[][] = { { 1, 0, 1 }, { 1, 1, 1 }, { 1, 0, 0 } };
+	int matriz[][] = { { 1, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 } };
 
 	int contadorDeUnos = 0;
 
@@ -20,13 +21,39 @@ public class AnalisisCeldas {
 			}
 			if (siTengoColumnaALaDerecha(columna)) {
 				sumarArribaALaDerecha(fila, columna);
-
 			}
 			sumarElValorQueTengoArriba(fila, columna);
 		}
+		Assert.assertEquals(2, contadorDeUnos);
 
-		System.out.println(contadorDeUnos);
+		if (siTengoColumnaALaIzquierda(columna)) {
+			contadorDeUnos += matriz[fila][columna - 1];
+			Assert.assertEquals(3, contadorDeUnos);
+		}
 
+		if (siTengoColumnaALaDerecha(columna)) {
+			contadorDeUnos += matriz[fila][columna + 1];
+			Assert.assertEquals(4, contadorDeUnos);
+		}
+
+		if (siTengoFilaAbajo(fila)) {
+			if (siTengoColumnaALaIzquierda(columna)) {
+				contadorDeUnos += matriz[fila + 1][columna - 1];
+			}
+			Assert.assertEquals(5, contadorDeUnos);
+
+			if (siTengoColumnaALaDerecha(columna)) {
+				contadorDeUnos += matriz[fila + 1][columna + 1];
+			}
+			Assert.assertEquals(6, contadorDeUnos);
+			contadorDeUnos += matriz[fila + 1][columna];
+			Assert.assertEquals(6, contadorDeUnos);
+		}
+
+	}
+
+	private boolean siTengoFilaAbajo(int fila) {
+		return (fila + 1) <= matriz.length;
 	}
 
 	private void sumarElValorQueTengoArriba(int fila, int columna) {
@@ -38,7 +65,7 @@ public class AnalisisCeldas {
 	}
 
 	private boolean siTengoColumnaALaDerecha(int columna) {
-		return columna + 1 > matriz.length;
+		return columna + 1 <= matriz.length;
 	}
 
 	private void sumarValorDeArribaALaIzquierda(int fila, int columna) {
@@ -46,7 +73,7 @@ public class AnalisisCeldas {
 	}
 
 	private boolean siTengoColumnaALaIzquierda(int columna) {
-		return (columna - 1) > 0;
+		return (columna - 1) >= 0;
 	}
 
 	private boolean siTengoFilaArriba(int fila) {
